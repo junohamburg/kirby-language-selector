22085d723d26c95e4a7d8dbba444d25e486e38bd
Sebastian Eberlein <sebastian@juno-hamburg.com>
2023-11-15 12:12:53 +0100

    Add plugin

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+Kirby::plugin('junohamburg/language-selector', [
+  'hooks' => require __DIR__ . '/config/hooks.php',
+  'api' => require __DIR__ . '/config/api.php',
+  'translations' => A::keyBy(A::map(
+    Dir::read(__DIR__ . '/translations'),
+    function ($file) {
+      $translations = Yaml::decode(F::read(__DIR__ . '/translations/' . $file));
+      $translations['lang'] = F::name($file);
+
+      // Add plugin prefix to translations
+      $pluginTranslations = [];
+
+      foreach ($translations as $key => $value) {
+        $pluginTranslations['junohamburg.language-selector.' . $key] = $value;
+      }
+
+      return $pluginTranslations;
+    }
+  ), 'junohamburg.language-selector.lang')
+]);
