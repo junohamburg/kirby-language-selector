3c0d705b040dafe38a5a32a4731628a21acb5c6c
Sebastian Eberlein <sebastian@juno-hamburg.com>
2023-12-01 14:51:17 +0100

    Add Kirby version detection

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+use Kirby\Cms\App as Kirby;
+use Composer\Semver\Semver;
+
+// Validate Kirby version
+if (Semver::satisfies(Kirby::version() ?? '0.0.0', '~4.0') === false) {
+    throw new Exception('The language selector plugin requires Kirby 4');
+}
+
 Kirby::plugin('junohamburg/language-selector', [
   'options' => [
     'allowDelete' => true,
